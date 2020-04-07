ad3a3912696942e8a14a9205b3c109a7644a6fe3
EkaPratama99 <61812376+EkaPratama99@users.noreply.github.com>
2020-04-07 09:52:09 +0800

    Part 7 (RALAT)
    part 7 yang sebelumya salah upload
    
    1801010024

---
+++ Part 7 (RALAT)/index.js
@@ -0,0 +1,141 @@
+const electron = require("electron");
+const { v4 : uuidv4 } = require('uuid');
+uuidv4();
+const {app,
+     BrowserWindow,
+     Menu,
+     ipcMain
+    } = electron;
+
+let todayWindow;
+let createWindow;
+let listWindow;
+let aboutWindow;
+
+let allAppointment = [];
+
+app.on("ready", ()=> {
+    todayWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        title : "Aplikasi Dokter"
+    });
+
+    todayWindow.loadURL(`file://${__dirname}/today.html`);
+    todayWindow.on("closed", () => {
+    
+        app.quit();
+
+        todayWindow = null;
+    });
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
+
+});
+
+const listWindowCreator = () => {
+    listWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        geight: 400,
+        title: "All Appointments"
+    });
+
+    listWindow.setMenu=(null);
+    listWindow.loadURL(`file://${__dirname}/list.html`);
+    listWindow.on("closed", () => (listWindow = null));
+};
+const createWindowCreator = () => {
+    createWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        geight: 400,
+        title: "Create Appointments"
+    });
+
+    createWindow.setMenu=(null);
+    createWindow.loadURL(`file://${__dirname}/create.html`);
+    createWindow.on("closed", () => (createWindow = null));
+};
+
+const aboutWindowCreator = () => {
+    aboutWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        geight: 400,
+        title: "Create Appointments"
+    });
+
+    aboutWindow.setMenu=(null);
+    aboutWindow.loadURL(`file://${__dirname}/about.html`);
+    aboutWindow.on("closed", () => (aboutWindow = null));
+};
+
+ipcMain.on("appointment:create", (event, appointment) => {
+    appointment["id"] = uuidv4();
+    appointment["done"] = 0;
+    allAppointment.push(appointment);
+    createWindow.close();
+
+    console.log(allAppointment)
+});
+
+ipcMain.on("appointment:request:list", event => {
+    listWindow.webContents.send('appointment:response:list', allAppointment);
+});
+
+ipcMain.on("appointment:request:today", event => {
+    console.log("here2");
+});
+
+ipcMain.on("appointment:done", (event, id) => {
+    console.log("here3")
+});
+
+
+
+const menuTemplate = [{
+    label: "File",
+    submenu: [{
+        label: "New Appointment",
+
+        click() {
+            createWindowCreator();
+        }
+    },
+    {
+    label: "All Appointment",
+    click() {
+        listWindowCreator();
+    }
+    },
+    {
+        label: "Quit",
+        click() {
+            app.quit();
+        }
+        }
+    ]
+},
+
+{
+    label: "View",
+    submenu: [{ role: "reload" }, { role: "toggledevtools" }]
+},
+{
+    label: "About",
+    click(){
+        aboutWindowCreator();
+    }
+}
+
+]
